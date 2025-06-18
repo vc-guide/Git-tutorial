e90d2e513ccedf75b68c06ad7c124498899d26d0
Vinayan C L <vinaychristo.vc@gmail.com>
2025-06-18 20:47:51 +0530

    Version 3

--- cardproject/src/components/Hello.jsx
+++ cardproject/src/components/Hello.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const Hello = () => {
+  const name =["vini", "babu", "raju"]
   return (
     <div>Hello
       <h1>Myre Idapadu</h1>
+      <section>
+        {name.map(n=>(
+          <li>{n}</li>
+        ))}
+      </section>
     </div>
   )
 }
